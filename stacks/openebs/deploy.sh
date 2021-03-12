12c455e786a23cf75413915db1731cbc665d597f
Naresh Deshaveni <naresh.deshaveni@mayadata.io>
2021-03-12 15:47:02 +0530

    Updated Openebs version to 2.6.0 Changed the helm repo from stable/openebs to https://openebs.github.io/charts

--- stacks/openebs/deploy.sh
+++ stacks/openebs/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 STACK="openebs"
-CHART="stable/openebs"
-CHART_VERSION="1.10.0"
+CHART="openebs/openebs"
+CHART_VERSION="2.6.0"
 NAMESPACE="openebs"
 
 if [ -z "${MP_KUBERNETES}" ]; then
@@ -12,6 +12,10 @@ if [ -z "${MP_KUBERNETES}" ]; then
 else
   VALUES="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/${STACK}/values.yaml"
 fi
+
+
+helm repo add openebs https://openebs.github.io/charts
+helm repo update
 
 helm install "$CHART" \
   --create-namespace \
